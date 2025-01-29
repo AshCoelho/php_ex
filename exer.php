ad4257a06f82a695d935e186de6e5659d619a8f3
Ash Coelho <ashlakarina02@gmail.com>
2025-01-29 14:36:12 -0300

    exercicio_4

--- exer.php
+++ exer.php
@@ -7,6 +7,23 @@ while ($contador <= 10) {
     echo $contador . " ";
     $contador++;
 }
-*/
+
 2 - EXERCICIO: Mostre todos os números pares de 1 a 20.
+
+$contador = 2;
+while ($contador <=20 ) {
+    
+    echo $contador . " ";
+    $contador += 2;
+}
+
+3 - EXERCICIO: Exiba todos os números ímpares entre 1 e 15.
+
+$contador = 1;
+while ($contador <=15) {
+    echo $contador . "";
+    $contador += 2;
+}
+*/
+4 - EXERCICIO: Exiba a tabuada do número 5 usando while.
 ?>
