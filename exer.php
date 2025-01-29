3a91a34a60b82371782fb8fb28f5d30940b83279
Ash Coelho <ashlakarina02@gmail.com>
2025-01-29 14:29:39 -0300

    exeercicio1

---
+++ exer.php
@@ -0,0 +1,12 @@
+<?php 
+/*
+1 - EXERCICIO: Escreva um programa que imprima os números de 1 a 10 usando while.
+
+$contador = 1;
+while ($contador <= 10) {
+    echo $contador . " ";
+    $contador++;
+}
+*/
+2 - EXERCICIO: Mostre todos os números pares de 1 a 20.
+?>
